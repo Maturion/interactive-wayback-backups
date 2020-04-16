530dae35e88fbc1809b0099c529c893381aea85e
Matthias Knoll <matthias.knoll@maturion.de>
2020-04-16 13:30:22 +0200

    customizations for phpBB-based forums

--- _interactive_php_archive.php
+++ _interactive_php_archive.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-Type: text/html; charset=ISO-8859-15");
 $filedir = basename($_SERVER['PHP_SELF'], ".php");
-$filename = "content-php";
+$filename = $filedir . "-php";
 
 
 $i = 0;
@@ -20,13 +20,23 @@ foreach($_GET as $param => $value) {
 $finalfilename = ".phparchiv/" . $filedir . "/" . $filename . ".txt";
 $filecontent = file_get_contents($finalfilename);
 
+
 if($filecontent === false) {
-    $filecontent = '<h1>Nicht gefunden</h1>';
+
+    /* Customizations for phpBB-based forums */
     if (strpos($finalfilename, '&amp') !== false) {
-        $filecontent = file_get_contents(substr($finalfilename, 0, -8) . '.txt');
-        $filecontent = ($filecontent !== false) ? $filecontent : '<h1>Nicht gefunden</h1>';
+        $finalfilename = (substr($finalfilename, 0, -8) . '.txt');
     }
+    if (strpos($finalfilename, 'sid=') !== false) {
+
+        $finalfilename = preg_replace('/(?<=sid=)(.*)(?=.txt|&)/', '', $finalfilename);
+        $sidreplacements = ['&sid=', '?sid='];
+        $finalfilename = str_replace($sidreplacements, '', $finalfilename);
+    }
+ 
+    $filecontent = file_get_contents($finalfilename);
+    $filecontent = ($filecontent !== false) ? $filecontent : '<h1>Nicht gefunden</h1>';
+    
 }
 
 echo $filecontent;
-
